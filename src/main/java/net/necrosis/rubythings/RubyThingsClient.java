afe8f8afbb145c3cab1180f7cffde2d6b63573ec
Necrow <endlessword65@gmail.com>
2024-10-28 09:03:09 -0600

    Initial commit

---
+++ src/main/java/net/necrosis/rubythings/RubyThingsClient.java
@@ -0,0 +1,10 @@
+package net.necrosis.rubythings;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RubyThingsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
