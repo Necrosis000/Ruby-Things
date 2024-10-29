0c329780cc7a8bbd4282a949cd17c477fda8c0f2
Necrow <endlessword65@gmail.com>
2024-10-28 18:01:12 -0600

    Added WorldGen for Ruby Ores

--- src/main/java/net/necrosis/rubythings/RubyThings.java
+++ src/main/java/net/necrosis/rubythings/RubyThings.java
@@ -2,6 +2,13 @@ package net.necrosis.rubythings;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import net.necrosis.rubythings.block.ModBlocks;
 import net.necrosis.rubythings.item.ModItemGroups;
 import net.necrosis.rubythings.item.ModItems;
@@ -12,11 +19,15 @@ import org.slf4j.LoggerFactory;
 public class RubyThings implements ModInitializer {
 	public static final String MOD_ID = "rubythings";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final RegistryKey<PlacedFeature> CUSTOM_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of("rubythings","ore_custom"));
+
+
 
 	@Override
 	public void onInitialize() {
 		ModItems.registerItems();
 		ModBlocks.registerBlocks();
 		ModItemGroups.registerItemGroups();
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, CUSTOM_ORE_PLACED_KEY);
 	}
 }
